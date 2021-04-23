b7b8126e4cd416fcc924e48a3fd7864023174045
Alex Pho <alexcpho@gmail.com>
2021-04-23 10:30:55 -0400

    mongoose and sample data (#1)
    * add missing field for comment message
    
    * update field to account for multiple comment ids for post
    
    * add dotenv and mongo atlas connection
    
    * allow db env included for local testing
    
    * init sample json data loaded into mongo atlas
    
    * dotenv to use to connect to mongo atlas
    
    Co-authored-by: Alex Pho <pho.a@husky.neu.edu>

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+// Relative path for dotenv: https://dev.to/eriesgo/dotenv-and-relative-paths-fp2
+dotenv.config({path:__dirname + `./loaders/.env`})
+
+mongoose.connect(
+    `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_URL}`,
+    {useNewUrlParser: true, useUnifiedTopology: true, ssl: true}
+)
+
 // Configures CORS
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
