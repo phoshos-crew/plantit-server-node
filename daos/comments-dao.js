fe30163a4d9728b9ed5cb0aff944fa4a03373222
Alex Pho <pho.a@husky.neu.edu>
2021-04-24 11:43:09 -0400

    add initial dao

--- daos/comments-dao.js
+++ daos/comments-dao.js
@@ -1,9 +1,43 @@
 const commentsModel = require('../models/comments/comments-model')
 
+const findAllComments = () => {
+    return commentsModel.find();
+}
+
 const findCommentsForUser = (userId) => {
-    return commentsModel.find({originalPoster: userId});
+    return commentsModel.findOne({originalPoster: userId})
+        .populate('comments')
+}
+
+const findCommentById = (cid) => {
+    return commentsModel.findById(cid)
+}
+
+const createComment = (userId, comment) => {
+    return commentsModel.create(
+        {
+            postId: comment.postId, likedByUsers: comment.likedByUsers, originalPoster: comment.originalPoster,
+            body: comment.body
+        })
+}
+
+const updateComment = (cid, newBody) => {
+    return commentsModel.updateOne({_id: cid}, {$set: {body: newBody}})
+}
+
+const deleteAllCommentsForUser = (userId) => {
+    return commentsModel.deleteMany({originalPoster: userId})
+}
+
+const deleteOneCommentForUser = (userId, cid) => {
+    return commentsModel.deleteOne({originalPoster: userId, _id: cid})
 }
 
 module.exports = {
-    findCommentsForUser
+    findAllComments,
+    findCommentsForUser,
+    findCommentById,
+    createComment,
+    deleteAllCommentsForUser,
+    deleteOneCommentForUser
 }
