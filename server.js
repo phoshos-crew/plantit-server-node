fedcb3a6923efcdaaa9650ba3debb7f27306ba82
Juan Francisco Alfaro <franciscoalfaro2008@gmail.com>
2021-04-23 21:13:48 -0400

    Feat/login and sessions (#3)
    * Creates login, logout and profile api endpoints
    
    * Fixed some issues with CORS
    
    Co-authored-by: Juan Alfaro <alfaro.j@husky.neu.edu>

--- server.js
+++ server.js
@@ -6,25 +6,38 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
 // Relative path for dotenv: https://dev.to/eriesgo/dotenv-and-relative-paths-fp2
-dotenv.config({path:__dirname + `./loaders/.env`})
+dotenv.config({path:`./loaders/.env`})
 
-mongoose.connect(
-    `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_URL}`,
-    {useNewUrlParser: true, useUnifiedTopology: true, ssl: true}
-)
+
+mongoose
+    .connect(`mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_URL}`,
+    {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('Database Connected'))
+    .catch(err => console.log(err))
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+const session = require('express-session')
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+}))
 
 // Configures CORS
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Headers',
         'Content-Type, X-Requested-With, Origin');
     res.header('Access-Control-Allow-Methods',
         'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Credentials", "true");
     next();
 });
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+require("./controllers/users-controllers")(app)
 
 app.listen(process.env.PORT || 3001, () => {
     console.log(`Server is listening...`);
