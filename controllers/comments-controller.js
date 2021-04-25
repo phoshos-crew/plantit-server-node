e5f8a9c1e70bae5b4f5dbac829a68c9994ce028e
Alex Pho <pho.a@husky.neu.edu>
2021-04-25 12:05:25 -0400

    fixed my rebase with comments

--- controllers/comments-controller.js
+++ controllers/comments-controller.js
@@ -1,7 +1,19 @@
 module.exports = (app) => {
     const commentsService = require('../services/comments-services')
 
-    app.get('/api/users/:userId/comments', (req, res) =>
-        commentsService.findCommentsForUser(req.params['userId'], res)
+    app.get('/api/posts/:postId/comments', (req, res) =>
+        commentsService.findCommentsForPost(req.params['postId'], res)
             .then(comments => res.send(comments)))
+
+    app.post('/api/posts/:postId/comments', (req, res) =>
+        commentsService.createComment(req.params['postId'], req.body)
+            .then(comment => res.send(comment)))
+
+    app.post('/api/comments/:commentId', (req, res) =>
+        commentsService.updateComment(req.params['commentId'], req.body)
+            .then(comment => res.send(comment)))
+
+    app.delete('/api/comments/:commentId', (req, res) =>
+        commentsService.deleteComment(req.params['commentId'])
+            .then(comment => res.send(comment)))
 }
