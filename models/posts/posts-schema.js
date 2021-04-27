9e769c67da89d932aa88123d641fdbc8ffe0ecd5
Alex Pho <pho.a@husky.neu.edu>
2021-04-27 07:35:48 -0400

    add timestamp for new objects, and default types for identification

--- models/posts/posts-schema.js
+++ models/posts/posts-schema.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const postsSchema = mongoose.Schema(
     {
         _id: String,
+        type: {type: String, default: 'post'},
         imageUrl: String,
         likedByUsers: [{
                 type: String,
@@ -14,7 +15,8 @@ const postsSchema = mongoose.Schema(
         commentIds: [{
                 type: String,
                 ref: 'CommentsModel'
-        }]
-    }, {collection: "posts"})
+        }]},
+        { timestamps: true},
+    {collection: "posts"})
 
 module.exports = postsSchema
